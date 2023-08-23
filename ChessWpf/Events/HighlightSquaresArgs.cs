935f5bde84731cbac0bd5957c68041fe49dacf48
Jared Karuzis <chromedomere@gmail.com>
2023-08-22 22:45:11 -0400

    white space changes

--- ChessWpf/Events/HighlightSquaresArgs.cs
+++ ChessWpf/Events/HighlightSquaresArgs.cs
@@ -4,8 +4,10 @@ using System.Collections.Generic;
 namespace ChessWpf;
 
 public class HighlightSquaresArgs : EventArgs {
+
     public SquareButton clicked;
     public List<int[]> squares;
+
     public HighlightSquaresArgs(SquareButton o, List<int[]> s) {
         clicked = o;
         squares = s;
